7f409b6da24d1425712526e761a6c65f4ba94043
kimdaeun <de.kim@wconcept.co.kr>
2022-08-26 11:49:38 +0900

    add .gitignore and add aop

---
+++ member/member-core/src/main/java/org/member/core/aop/TimeTraceAop.java
@@ -0,0 +1,9 @@
+/**
+ *packageName    : org.member.core.aop
+ * fileName       : TimeTraceAop
+ * author         : dani
+ * date           : 2022/08/26
+ * description    :
+ */package org.member.core.aop;
+public class TimeTraceAop {
+}
