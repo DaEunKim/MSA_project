16db19059bdc5790aaeb1a6cdfda8f02abc5da43
kimdaeun <de.kim@wconcept.co.kr>
2022-08-26 11:43:34 +0900

    msa init

---
+++ member/member-api/src/main/java/org/member/api/Main.java
@@ -0,0 +1,21 @@
+package org.member.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * packageName    : org.member.api
+ * fileName       : ${NAME}
+ * author         : ${USER}
+ * date           : ${DATE}
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * ${DATE}        ${USER}       최초 생성
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello member-api!");
+    }
+}
---
+++ member/member-batch/src/main/java/org/member/batch/Main.java
@@ -0,0 +1,21 @@
+package org.member.batch;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * packageName    : org.member.batch
+ * fileName       : ${NAME}
+ * author         : ${USER}
+ * date           : ${DATE}
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * ${DATE}        ${USER}       최초 생성
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello member-batch!");
+    }
+}
