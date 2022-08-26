52d9c95721f2c704bcfd3e9daad02d7a6e1cffff
kimdaeun <de.kim@wconcept.co.kr>
2022-08-26 11:56:35 +0900

    .gradle

--- member/member-core/src/main/java/org/member/core/Main.java
+++ member/member-core/src/main/java/org/member/core/Main.java
@@ -13,6 +13,6 @@ package org.member.core;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello member-core!");
     }
 }
--- member/member-core/src/main/java/org/member/core/aop/TimeTraceAop.java
+++ member/member-core/src/main/java/org/member/core/aop/TimeTraceAop.java
@@ -1,9 +1,33 @@
 /**
- *packageName    : org.member.core.aop
+ * packageName    : org.member.core.aop
  * fileName       : TimeTraceAop
  * author         : dani
  * date           : 2022/08/26
  * description    :
- */package org.member.core.aop;
+ */
+package org.member.core.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class TimeTraceAop {
+    @Around("execution(* org.member..*(..))") // 어디에 사용할건지 공통 관심사를 타겟팅해준다.
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toShortString());
+
+        try {
+            return joinPoint.proceed(); // 다음 메서드로 진행
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+
+            System.out.println("END: " + joinPoint.toShortString() + " " + timeMs + "ms");
+        }
+    }
 }
